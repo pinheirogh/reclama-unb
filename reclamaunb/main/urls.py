3bd3f0c02ef6c0eca6381a7951c38225fac56e35
Gabriel Pinheiro <gabriel.pinheiroconceicao@gmail.com>
2023-01-09 14:54:33 -0300

    Adiciona urls genéricas

--- reclamaunb/main/urls.py
+++ reclamaunb/main/urls.py
@@ -6,4 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('dados/', HomePageView.as_view(), name='dados'),
+    path('reclamacoes/', HomePageView.as_view(), name='reclamacoes'),
+    path('reportar/', HomePageView.as_view(), name='reportar'),
 ]
