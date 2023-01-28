5ce6e61236d89eb02b9ad5ffcd2bf571ccf20e75
Gabriel Pinheiro <gabriel.pinheiroconceicao@gmail.com>
2023-01-28 17:19:15 -0300

    Altera settings.py

--- reclamaunb/reclamaunb/settings.py
+++ reclamaunb/reclamaunb/settings.py
@@ -120,9 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/home/pinheiropa/reclama-unb/reclamaunb/main/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
