dc59f0a3fcc6ed2b348daab3166bf63afc1b067a
Gabriel Pinheiro <gabriel.pinheiroconceicao@gmail.com>
2023-01-28 17:31:03 -0300

    Desliga debug em produção

--- reclamaunb/reclamaunb/settings.py
+++ reclamaunb/reclamaunb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-njd&p2dqq8_c4_ia83=$d5^k@=bif*^m_id7!7cnn!lu+(zi^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pinheiropa.pythonanywhere.com']
 
