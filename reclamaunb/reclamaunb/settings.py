503db889e83c39883ee395ead559a763112b9768
Gabriel Pinheiro <gabriel.pinheiroconceicao@gmail.com>
2023-01-31 17:38:56 -0300

    Remove componente de settings

--- reclamaunb/reclamaunb/settings.py
+++ reclamaunb/reclamaunb/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-njd&p2dqq8_c4_ia83=$d5^k@=bif*^m_id7!7cnn!lu+(zi^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
